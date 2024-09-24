b49f91563997e64fdca2fd858ac68d8f06999a6e
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-09-24 11:52:47 +0530

    first commit

---
+++ Addition.js
@@ -0,0 +1,10 @@
+function Addtwonumb(num1, num2) {
+    return num1 + num2
+}
+
+let number1= 10;
+let number2 = 20;
+
+let sum = Addtwonumb(number1, number2) ;
+
+console.log("The sum of " + number1 + " and " + number2 + " is: ", sum);
