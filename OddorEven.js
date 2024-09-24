4e55d09b992b40c69f4e1bd73dd9b1c1019b5a87
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-09-24 12:12:03 +0530

    Odd or even checker

---
+++ OddorEven.js
@@ -0,0 +1,12 @@
+//Check Even or Odd
+
+let number = 14;
+
+function OddorEven(number) {
+if(number % 2 === 0) {
+    console.log("The given number " + number +  " is Even")
+} else {
+   console.log("The given number " + number +  " is Odd")
+}
+}
+OddorEven(number);
