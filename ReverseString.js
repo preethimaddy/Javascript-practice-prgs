5ee8744a3f7cbba4dc7e2c43ee1e6cb5d4e07f9e
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-09-24 19:26:37 +0530

    Palindrome checker

--- ReverseString.js
+++ ReverseString.js
@@ -4,3 +4,17 @@ let str = "Helloooo";
 
 let rev = str.split('').reverse().join('');
 console.log("Desired O/p :", rev);
+
+
+// Palindrome checker
+
+function isPalindrome(name) {
+
+    let input = name.toString();
+
+let reverseIn = input.split('').reverse().join('');
+
+ return reverseIn === input;
+}
+console.log(isPalindrome("HANNAH"))
+
