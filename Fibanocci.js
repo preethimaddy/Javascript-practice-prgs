84a5e01c036b6ec5ae0d34409b6313c51deaddd1
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-09-24 19:50:22 +0530

    Fibanocci series generate

---
+++ Fibanocci.js
@@ -0,0 +1,13 @@
+//Fibanocci series
+
+let input =[0,1]
+
+function FibanocciSeries(n) {
+
+    for(let i=2; i<n; i++){
+
+        input.push(input[i-1] + input[i-2])
+    }
+    return input
+}
+console.log(FibanocciSeries(8))
