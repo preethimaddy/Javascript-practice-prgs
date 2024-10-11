1038a7dd706395fc0b79efa0191210f974425ca9
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-10-11 13:14:20 +0530

    Sort an Array of Objects by a Property

---
+++ SortByProp.js
@@ -0,0 +1,10 @@
+// Sort an Array of Objects by a Property
+
+let people = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 20 },
+    { name: 'Charlie', age: 23 }
+]
+
+let sort =((a,b)=> a.age - b.age)
+console.log(people)
