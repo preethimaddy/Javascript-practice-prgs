d428a6c5cc5762cba67807c65c589fe6a453466c
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-10-15 18:18:26 +0530

    Check if a String is a Substring of Another String

---
+++ isSubstring.js
@@ -0,0 +1,7 @@
+let string = " JavaScript";
+
+let subString = "Script";
+
+let isSubstring = string.includes(string);
+
+console.log(`"${subString}" is a substring of "${string}": ${isSubstring}`);
