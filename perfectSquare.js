cea27b60d5720e76706131a9a62dec4d62ec2a30
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-10-16 15:39:19 +0530

    perfectSquare.js

---
+++ perfectSquare.js
@@ -0,0 +1,7 @@
+// Check if a Number is a Perfect Square
+
+let num = 16;
+
+let isPerfectSquare = Number.isInteger(Math.sqrt(num));
+
+console.log(`${num} is a perfect square: ${isPerfectSquare}`);
